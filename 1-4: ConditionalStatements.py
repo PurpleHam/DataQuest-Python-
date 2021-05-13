7f7d7b75b71bfb7e53f1fa1c9dddd2508119e852
PurpleHam <76711933+PurpleHam@users.noreply.github.com>
2021-05-13 16:45:46 -0400

    Update 1-4: ConditionalStatements.py

--- 1-4: ConditionalStatements.py
+++ 1-4: ConditionalStatements.py
@@ -1,4 +1,36 @@
-opened_file = open('AppleStore.csv') # Open file
+# Compute the average rating of the apps that have a price greater than $9.
+# Find out how many apps have a price greater than $9 and assign the result to a variable named n_apps_more_9.
+# Find out how many apps have a price less than or equal to $9 and assign the result to a variable named n_apps_less_9.
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+above_9_ratings = [] # Initialize an empty list called above_9_ratings
+for row in apps_data[1:]: # For each row after the header row 
+	if float(row[4]) > 9: # If the price (4th indexed item) of the app is greater than $9
+		above_9_ratings.append(float(row[7])) # Append the corresponding rating to the above_9_ratings list   
+
+avg_rating = sum(above_9_ratings)/len(above_9_ratings) # Calculate average rating of apps that have a price of greater than $9 
+
+n_apps_more_9 = 0 # Initialize counter for apps that cost more than $9 
+for row in apps_data[1:]: 
+    if float(row[4]) > 9:
+        n_apps_more_9 = n_apps_more_9 + 1
+
+n_apps_less_9 = 0 # Initialize counter for apps that cost less than $9 
+for row in apps_data[1:]:
+    if float(row[4]) < 9:
+        n_apps_less_9 = n_apps_less_9 + 1
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Label each app as "free," "affordable," "expensive," or "very expensive" in a newly created column called price_label. 
+
+pened_file = open('AppleStore.csv') # Open file
 from csv import reader # Import reader
 read_file = reader(opened_file) # Read file
 apps_data = list(read_file) # Convert file to a list
